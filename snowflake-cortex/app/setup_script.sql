2e9fc31a926e1a93079cdcef0cb48587fa1851df
Ricardo Alfaro Viquez <ricardo.alfaroviquez@snowflake.com>
2024-06-19 12:35:55 -0600

    draft for cortex, missing db integrations and several improvements

--- snowflake-cortex/app/setup_script.sql
+++ snowflake-cortex/app/setup_script.sql
@@ -8,25 +8,12 @@ CREATE APPLICATION ROLE IF NOT EXISTS app_public;
 -- 2. Create a versioned schema to hold those UDFs/Stored Procedures
 CREATE OR ALTER VERSIONED SCHEMA core;
 GRANT USAGE ON SCHEMA core TO APPLICATION ROLE app_public;
--- 3. Create a view referencing the shared view in the package shared-content.sql.
-CREATE VIEW IF NOT EXISTS core.shared_view
-AS SELECT * FROM package_shared.PROVIDER_SPOTIFY_VIEW;
+-- -- 3. Create a view referencing the shared view in the package shared-content.sql.
+-- CREATE VIEW IF NOT EXISTS core.shared_view
+-- AS SELECT * FROM package_shared.PROVIDER_SPOTIFY_VIEW;
 
--- 4. Grant appropriate privileges over the view to your application roles. 
-GRANT SELECT ON VIEW core.shared_view TO APPLICATION ROLE app_public;
-
--- GRANT DATABASE ROLE SNOWFLAKE.CORTEX_USER TO APPLICATION ROLE app_public;
-
--- CREATE OR REPLACE PROCEDURE CORE.CORTEX_TEXT()
--- RETURNS STRING
--- LANGUAGE SQL
--- AS $$
---      BEGIN
---           RETURN (SELECT SNOWFLAKE.CORTEX.COMPLETE('snowflake-arctic', 'What are large language models?'));
---      END;
--- $$;
-
--- GRANT USAGE ON PROCEDURE CORE.CORTEX_TEXT() TO APPLICATION ROLE app_public;
+-- -- 4. Grant appropriate privileges over the view to your application roles. 
+-- GRANT SELECT ON VIEW core.shared_view TO APPLICATION ROLE app_public;
 
 -- 3. Create a streamlit object using the code you wrote in you wrote in src/module-ui, as shown below. 
 -- The `from` value is derived from the stage path described in snowflake.yml
--- snowflake-cortex/src/ui/ui.py
+++ snowflake-cortex/src/ui/ui.py
@@ -1,27 +1,31 @@
+import snowflake.snowpark.functions as F
+
+def call_cortex(df, input: str):
+   sample_df = df.to_pandas().to_string()
+   input_with_table = f"""The following data is a table that contains songs information it is wrapped in this tags <dataset>{sample_df}</dataset> {input}"""
+   new_df = df.with_column("CORTEX", F.call_builtin("SNOWFLAKE.CORTEX.COMPLETE", F.lit('llama2-70b-chat'), F.lit(input_with_table))).collect()
+   return new_df
+
 def run_streamlit():
    # Import python packages
    # Streamlit app testing framework requires imports to reside here
    import pandas as pd
    import streamlit as st
-   from snowflake.snowpark.functions import call_udf, col, call_builtin, lit
    from snowflake.snowpark import Session
-   from snowflake.cortex import Complete, Summarize
-
-   st.title('Hello Snowflake!')
-
 
    st.header('Snowflake Cortex Example')
 
    # Get the current credentials
    session = Session.builder.getOrCreate()
 
-   spotify_df = session.table('core.shared_view')
-   st.write("intento")
-   st.dataframe(spotify_df)
-
-   cortex_fn = spotify_df.with_column("CORTEX",call_builtin("SNOWFLAKE.CORTEX.COMPLETE", lit('llama2-70b-chat'), lit("What are large language models?")))
-   st.write(cortex_fn)
-
+   df1 = session.create_dataframe([[1, 2, 3, 4, 5]], schema=["number1","number2","number3","number4","number5"])
+   df1.write.save_as_table("my_table", mode="overwrite")
+   table_df = session.table("my_table")
+   st.write(table_df)
+   cortex_input = st.text_input("What do you want to ask to cortex about the table from above?")
+   if cortex_input is not "":
+      cortex_fn = call_cortex(table_df, cortex_input)
+      st.write(cortex_fn)
 
 if __name__ == '__main__':
    run_streamlit()
