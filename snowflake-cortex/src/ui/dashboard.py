215bc81799edfbd8ca8532f2ddca10ac3fc93710
Oscar Salazar <oscar.salazarlizano@snowflake.com>
2024-06-24 15:59:57 -0600

    fix test

--- snowflake-cortex/src/ui/dashboard.py
+++ snowflake-cortex/src/ui/dashboard.py
@@ -4,23 +4,22 @@ from cortexCaller import CortexCaller
 
 class Dashboard:
    
-   def __init__(self, session: Session = None) -> None:
-      self.session = session if session else Session.builder.getOrCreate()
+   def __init__(self, session: Session, cortex_caller: CortexCaller) -> None:
+      self.session = session
+      self.cortex_caller = cortex_caller
       
    def run_streamlit(self):
-      import streamlit as st
-
       st.header('Snowflake Cortex Example')
       st.subheader("Simple app showing how cortex can answer questions using the following spotify's ranking table")
 
-      table_df = self.session.table("PACKAGE_SHARED.PROVIDER_SPOTIFY_VIEW")
+      table_df = self.session.table("PACKAGE_SHARED.PROVIDER_SPOTIFY_VIEW").to_pandas()
       st.write(table_df)
 
       cortex_chat = st.container(height=200)
       cortex_input = st.chat_input("What do you want to ask to cortex about the table from above?", key="cortex_input")
       if cortex_input and not cortex_input.isspace():
          cortex_chat.chat_message("user").write(cortex_input)
-         cortex_fn = CortexCaller.call_cortex(self.session, table_df, cortex_input)
+         cortex_fn = self.cortex_caller.call_cortex(table_df, cortex_input)
          cortex_chat.chat_message("assistant").write(cortex_fn)
 
 if __name__ == '__main__':
--- snowflake-cortex/test/test_ui.py
+++ snowflake-cortex/test/test_ui.py
@@ -1,4 +1,5 @@
 from dashboard import Dashboard
+from cortexCaller import CortexCaller
 from streamlit.testing.v1 import AppTest
 from snowflake.snowpark import Session
 from unittest.mock import patch, MagicMock
@@ -10,15 +11,27 @@ def session():
     yield session
     session.close()
 
-# @patch('cortexCaller.CortexCaller.call_cortex', autospec=True)
-def test_cortex_call_mock(call_cortex: MagicMock, session):
-    dashboard = Dashboard(session)
-    at = AppTest.from_function(dashboard.run_streamlit).run()
+@pytest.fixture(autouse=True)
+def cortex_caller():
+    cortex_caller = CortexCaller()
+    yield cortex_caller
+
+@patch('cortexCaller.CortexCaller.call_cortex')
+@patch('snowflake.snowpark.session.Session.table')
+def test_cortex_call_mock(table: MagicMock, call_cortex: MagicMock, session, cortex_caller):
+    
+    def script(session, cortex_caller):
+        from dashboard import Dashboard
+        dashboard = Dashboard(session, cortex_caller)
+        return dashboard.run_streamlit()
+    
+    table.return_value=session.create_dataframe([{"TestColumn": "TestValue"}])
     call_cortex.return_value='Mocked response from cortex'
+    at = AppTest.from_function(script, kwargs={ "session": session, "cortex_caller": cortex_caller }).run()
+    
     at.chat_input(key='cortex_input').set_value('text for testing purposes').run()
     assert at.chat_message[0].name == 'user'
     assert at.chat_message[1].name == 'assistant'
-    assert call_cortex.assert_called_once()
 
 def test_input_showed_in_chat():
     dashboard = Dashboard(session)
